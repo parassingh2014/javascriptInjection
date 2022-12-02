3ae5a9690754de7dab90a2f2c1c1dd1b405644c6
Ramandeep Marwaha <43007109+parassingh2014@users.noreply.github.com>
2022-12-02 02:29:04 -0700

    Updated code

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-document.cookie = "rmarwaha@noemail.com"
-document.cookie = "PassWord"
+document.cookie = "username=rmarwaha@noemail.com";
+document.cookie = "password=afe_password";
 if (document.readyState == 'loading') {
 	document.addEventListener('DOMContentLoaded', ready)
 } else {
@@ -7,12 +7,19 @@ if (document.readyState == 'loading') {
 }
 
 function ready() {
-	var query = new URL(window.location).searchParams.get('query1')
-    var query2 = new URL(window.location).searchParams.get('query2')
-    var query3 = new URL(window.location).searchParams.get('query3')
-	document.getElementById('query-input1').value = query;
-    document.getElementById('query-input2').value = query2;
-	document.getElementById('query-output').innerHTML = query + " Name: " + query2;
-    localStorage.setItem("Query", query);
-    localStorage.setItem("Query2", query2);    
+	var query = new URL(window.location).searchParams.get('query')
+   // var query2 = new URL(window.location).searchParams.get('query2')
+    // var query3 = new URL(window.location).searchParams.get('query3')
+	document.getElementById('query-input').value = query;
+    //document.getElementById('query-input2').value = query2;
+	document.getElementById('query-output').innerHTML = query //+ "  " + query2;
+    localStorage.setItem("Number", query);
+    //localStorage.setItem("InstituitionName", query2);    
 }
+/* <img src onerror="var username = localStorage.getItem('Number'); console.log(username);">
+<img src onerror="var username = localStorage.getItem('Number'); console.log(username);">
+<a href ="#" onclick="alert(JSON.stringify(localStorage))">Click</a>
+
+<img src onerror="console.log(document.cookie)"> */
+
+/* <a href ="#" onclick="alert(JSON.stringify(localStorage))">Click</a> */
